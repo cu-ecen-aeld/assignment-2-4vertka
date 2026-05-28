974d8aa2069001864a86b6a9a4c5375c169f0536
Oleksandr <sashapisarchuk@gmail.com>
2026-05-28 23:14:53 +0300

    assignment 2

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -2,33 +2,32 @@
 #include <string.h>
 #include <syslog.h>
 
-
 int main(int argc, char **argv) {
-	int writefile_length = strlen(argv[1]);
-	int writestr_length = strlen(argv[2]);
+    openlog("writer", LOG_CONS, LOG_USER);
 
-	openlog("Log:", LOG_CONS, LOG_USER);
-	
-	if (writefile_length <= 0 || writestr_length <= 0) {
-		syslog(LOG_ERR, "Arguments were not specified");	
-		return 1;
-	}
+    if (argc < 3) {
+        syslog(LOG_ERR, "Usage: writer <file> <string>");
+        closelog();
+        return 1;
+    }
 
-	FILE *fp = fopen(argv[1], "w");
-	if (fp == NULL) {
-		syslog(LOG_ERR, "File (%s) cannot be opened/created", argv[1]);
-		fclose(fp);
-		return 1;
-	}
+    FILE *fp = fopen(argv[1], "w");
+    if (fp == NULL) {
+        syslog(LOG_ERR, "File (%s) cannot be opened/created", argv[1]);
+        closelog();
+        return 1;
+    }
 
-	if (fputs(argv[2], fp) != EOF) {
-		syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
-	} else {
-		syslog(LOG_ERR, "cannot write string (%s) to file (%s)", argv[2], argv[1]);
-		fclose(fp);
-		return 1;
-	}
+    if (fputs(argv[2], fp) != EOF) {
+        syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
+    } else {
+        syslog(LOG_ERR, "Cannot write string (%s) to file (%s)", argv[2], argv[1]);
+        fclose(fp);
+        closelog();
+        return 1;
+    }
 
-	fclose(fp);
-	closelog();
+    fclose(fp);
+    closelog();
+    return 0;
 }
